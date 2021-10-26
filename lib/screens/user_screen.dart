cb70ed159d2c1be950628af859f8cb57b523e0ab
Frabbi727 <Frabbi727@gmail.com>
2021-10-27 03:23:36 +0600

    add FOB for add product list using ListView

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:flutter/rendering.dart';
 
 class UserScreen extends StatefulWidget {
   UserScreen({Key? key}) : super(key: key);
@@ -11,6 +11,69 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreenState extends State<UserScreen> {
+  bool isFabVisibale = false;
+  void addNewProducts(BuildContext ctx) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+     
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+          child: Form(
+            
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              addRepaintBoundaries: true,
+              children: <Widget>[
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Product Name',
+                      icon: Icon(Icons.insert_chart)),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Product Description',
+                      icon: Icon(Icons.description)),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Minimum Bid Price',
+                      icon: Icon(Icons.price_check)),
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Auction End DateTime',
+                      icon: Icon(Icons.data_saver_off)),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Add Product'),
+                    ),
+                    ElevatedButton(onPressed: (){
+                     Navigator.push(context,  MaterialPageRoute(builder: (context)=>UserScreen()));
+                    }, child: Text('Cancel'))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,30 +105,67 @@ class _UserScreenState extends State<UserScreen> {
                 FirebaseAuth.instance.signOut();
               }
             },
-          )
+          ),
+
+          // DropdownButton(
+          //   items: [
+          //     DropdownMenuItem(
+          //       child: Container(
+          //         child: Row(
+          //           children: <Widget>[
+          //             Icon(
+          //               Icons.add,
+          //               color: Theme.of(context).indicatorColor,
+          //             ),
+          //             SizedBox(
+          //               width: 8,
+          //             ),
+          //             Text('Add New Items')
+          //           ],
+          //         ),
+          //       ),
+          //       value: 'AddProduct',
+          //     ),
+          //   ],
+          //   onChanged: (itemIdentifire) {
+          //     if (itemIdentifire == 'AddProduct') {
+          //       addNewProducts(context);
+          //     }
+          //   },
+          // ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This Works!'),
+      body: NotificationListener<UserScrollNotification>(
+        onNotification: (notification) {
+          if (notification.direction == ScrollDirection.forward) {
+            setState(() {
+              isFabVisibale = true;
+            });
+          }
+          if (notification.direction == ScrollDirection.reverse) {
+            setState(() {
+              isFabVisibale = false;
+            });
+          }
+          return true;
+        },
+        child: ListView.builder(
+          itemCount: 100,
+          itemBuilder: (ctx, index) => Container(
+            padding: EdgeInsets.all(8),
+            child: Text('This Works!'),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          FirebaseFirestore.instance
-              .collection('chats/PCnifwmYhlvCPvABc65t/messages')
-              .snapshots()
-              .listen((data) {
-                data.docs.forEach((docment) {
-                  print(docment['text']);
-                 });
-            
-          },); 
-        },
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: isFabVisibale
+          ? FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () {
+                addNewProducts(context);
+              },
+            )
+          : null,
     );
   }
 }
